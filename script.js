f5591c35cf2f5b4528be71d2262c3999d120abc3
shaun0073 <linru0073@gmail.com>
2026-03-20 13:53:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -342,7 +342,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             parts: [{ text: userSaid }]
                         });
 
-                        fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
+                        fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`, {
                             method: 'POST',
                             headers: { 'Content-Type': 'application/json' },
                             body: JSON.stringify({
